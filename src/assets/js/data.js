5b0c40ebc073a5ecebda32e44131882079b89ae5
Alberrt24 <narutobleach1231@homail.com>
2023-11-28 23:13:01 -0500

    first commit

---
+++ src/assets/js/data.js
@@ -0,0 +1,67 @@
+const menu = [
+  {
+    name: "Cecina de Chancho",
+    price: "15",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    special: true
+  },
+  {
+    name: "Sandwich de pollo",
+    price: "20",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Cecina de Res",
+    price: "19",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    special: true
+  },
+  {
+    name: "Asado con yucas",
+    price: "18",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Bistec a lo Pobre",
+    price: "17",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    special: true
+  },
+  {
+    name: "Cafe",
+    price: "16",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Jugo de fruta",
+    price: "15",
+    type: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Mote",
+    price: "14",
+    type: "dessert",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Caldo de Gallina",
+    price: "13",
+    type: "dessert",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    name: "Hamburguesa de res",
+    price: "12",
+    type: "dessert",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  }
+];
+
+export { menu };
